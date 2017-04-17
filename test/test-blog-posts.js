d1067d045776759d9012f0df51776ccca9993389
Jen Von Essen <cgajen09@gmail.com>
2017-04-17 14:10:44 -0700

    tests passing

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -3,8 +3,6 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
-// this makes the should syntax available throughout
-// this module
 const should = chai.should();
 
 const {DATABASE_URL} = require('../config');
@@ -14,10 +12,6 @@ const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
-// this function deletes the entire database.
-// we'll call it in an `afterEach` block below
-// to ensure  ata from one test does not stick
-// around for next one
 function tearDownDb() {
   return new Promise((resolve, reject) => {
     console.warn('Deleting database');
@@ -28,11 +22,6 @@ function tearDownDb() {
 }
 
 
-// used to put randomish documents in db
-// so we have data to work with and assert about.
-// we use the Faker library to automatically
-// generate placeholder values for author, title, content
-// and then we insert that data into mongo
 function seedBlogPostData() {
   console.info('seeding blog post data');
   const seedData = [];
@@ -46,7 +35,6 @@ function seedBlogPostData() {
       content: faker.lorem.text()
     });
   }
-  // this will return a promise
   return BlogPost.insertMany(seedData);
 }
 
@@ -62,8 +50,6 @@ describe('blog posts API resource', function() {
   });
 
   afterEach(function() {
-    // tear down database so we ensure no state from this test
-    // effects any coming after.
     return tearDownDb();
   });
 
@@ -71,37 +57,25 @@ describe('blog posts API resource', function() {
     return closeServer();
   });
 
-  // note the use of nested `describe` blocks.
-  // this allows us to make clearer, more discrete tests that focus
-  // on proving something small
   describe('GET endpoint', function() {
 
     it('should return all existing posts', function() {
-      // strategy:
-      //    1. get back all posts returned by by GET request to `/posts`
-      //    2. prove res has right status, data type
-      //    3. prove the number of posts we got back is equal to number
-      //       in db.
       let res;
       return chai.request(app)
         .get('/posts')
         .then(_res => {
           res = _res;
           res.should.have.status(200);
-          // otherwise our db seeding didn't work
           res.body.should.have.length.of.at.least(1);
 
           return BlogPost.count();
         })
         .then(count => {
-          // the number of returned posts should be same
-          // as number of posts in DB
           res.body.should.have.length.of(count);
         });
     });
 
     it('should return posts with right fields', function() {
-      // Strategy: Get back all posts, and ensure they have expected keys
 
       let resPost;
       return chai.request(app)
@@ -117,8 +91,6 @@ describe('blog posts API resource', function() {
             post.should.be.a('object');
             post.should.include.keys('id', 'title', 'content', 'author', 'created');
           });
-          // just check one of the posts that its values match with those in db
-          // and we'll assume it's true for rest
           resPost = res.body[0];
           return BlogPost.findById(resPost.id).exec();
         })
@@ -131,10 +103,6 @@ describe('blog posts API resource', function() {
   });
 
   describe('POST endpoint', function() {
-    // strategy: make a POST request with data,
-    // then prove that the post we get back has
-    // right keys, and that `id` is there (which means
-    // the data was inserted into db)
     it('should add a new blog post', function() {
 
       const newPost = {
@@ -156,7 +124,6 @@ describe('blog posts API resource', function() {
           res.body.should.include.keys(
             'id', 'title', 'content', 'author', 'created');
           res.body.title.should.equal(newPost.title);
-          // cause Mongo should have created id on insertion
           res.body.id.should.not.be.null;
           res.body.author.should.equal(
             `${newPost.author.firstName} ${newPost.author.lastName}`);
@@ -174,11 +141,6 @@ describe('blog posts API resource', function() {
 
   describe('PUT endpoint', function() {
 
-    // strategy:
-    //  1. Get an existing post from db
-    //  2. Make a PUT request to update that post
-    //  3. Prove post returned by request contains data we sent
-    //  4. Prove post in db is correctly updated
     it('should update fields you send over', function() {
       const updateData = {
         title: 'cats cats cats',
@@ -220,11 +182,6 @@ describe('blog posts API resource', function() {
   });
 
   describe('DELETE endpoint', function() {
-    // strategy:
-    //  1. get a post
-    //  2. make a DELETE request for that post's id
-    //  3. assert that response has right status code
-    //  4. prove that post with the id doesn't exist in db anymore
     it('should delete a post by id', function() {
 
       let post;
@@ -241,10 +198,6 @@ describe('blog posts API resource', function() {
           return BlogPost.findById(post.id);
         })
         .then(_post => {
-          // when a variable's value is null, chaining `should`
-          // doesn't work. so `_post.should.be.null` would raise
-          // an error. `should.be.null(_post)` is how we can
-          // make assertions about a null value.
           should.not.exist(_post);
         });
     });
